5b8dd0eac6f5b0f7096e1ed6f3a2c44944434943
abinaya0101 <82209374+abinaya0101@users.noreply.github.com>
2021-07-24 00:33:22 +0800

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -153,14 +153,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data=="help":
         buttons = [
             [
-                InlineKeyboardButton("🔥 Source Code 🔥", url='https://github.com/LushaiMusic/VCMusicPlayer'),
+                InlineKeyboardButton("🔥 Telegram 🔥", url='https://t.me/bcddgblg'),
             ],
             [
-               InlineKeyboardButton('👥 Group', url='https://t.me/iZaute/5'),
-               InlineKeyboardButton('Channel 📢', url='https://t.me/iZaute/6'),
+               InlineKeyboardButton('👥 Jangan Tap'),
+               InlineKeyboardButton('Yang Ini Juga Jangan Di Tap 📢'),
             ],
             [
-               InlineKeyboardButton('🔰 How to Deploy 🔰', url='https://t.me/c/1481808444/131'),
+               InlineKeyboardButton('🔰 Deploy 🔰', url='https://t.me/bcddgblg'),
         
             ]
         ]
